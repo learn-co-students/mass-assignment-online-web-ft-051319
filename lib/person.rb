dc6422fefe7b94f785d182fb2baacd9ffdb3a378
github username <github email address>
2019-05-30 11:59:43 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,8 +3,15 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
 
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
+  def initialize(attributes) #accepts attributes hash as an argument
+    attributes.each {|key, value| self.send(("#{key}="), value)} #send allows us to use a dynamic value for the key
   end
 
 end
+
+# susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+#
+# susan = Person.new(susan_attributes)
+# susan.name      # => "Susan"
+# susan.height    # => "5'11""
+# susan.eye_color # => "Green"
